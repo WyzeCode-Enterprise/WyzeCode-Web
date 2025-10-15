c73982af800de4a6a6dd34165e2286c607a624c9
drkcph <murilogiroldo.8b@gmail.com>
2025-10-15 01:27:24 -0300

    update

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export", // ativa exportação estática
   eslint: {
-    ignoreDuringBuilds: true, // ignora ESLint no build
+    ignoreDuringBuilds: true,
   },
 };
 
